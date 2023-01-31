10f74b3829bdeb5bacbd4753a5fa3ea517c2fec7
Tyler Brechner <tylerbrechner@gmail.com>
2023-01-31 16:30:44 -0500

    Set junction values

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2023-01-27T13:43:33.311-0500";
+  public static final String APP_BUILD_TIME = "2023-01-31T16:15:16.859-0500";
 }
